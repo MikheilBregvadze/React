9002f3b1e52180cfd2e63bd64e0042abca4d4e73
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2019-02-18 18:46:14 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layouts/Header'
+import uuid from 'uuid'
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: 'Angular',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Vue',
+        completed: true
+      },
+      {
+        id: uuid.v4(),
+        title: 'React',
+        completed: false
+      }
+    ]
+  }
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  deleteTodo = (id) => {
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
   render() {
+    console.log(this.state.todos[0].id);
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+        </div>
       </div>
     );
   }
